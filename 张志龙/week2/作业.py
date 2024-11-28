3e6d5192402d1073fc84d8bce631c7e6be7b224d
aharoya <166356577+aharoya@users.noreply.github.com>
2024-11-28 23:19:27 +0800

    Create 作业.py

---
+++ 张志龙/week2/作业.py
@@ -0,0 +1,107 @@
+# coding:utf8
+
+import torch
+import torch.nn as nn
+import numpy as np
+import random
+import json
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+class TorchModel(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(TorchModel, self).__init__()
+        self.linear = nn.Linear(input_size, output_size)  # 线性层
+        self.activation = nn.Softmax(dim=1)  # softmax归一化函数
+        # self.loss = nn.CrossEntropyLoss()  # loss函数采用交叉熵损失
+        self.loss = nn.functional.cross_entropy
+
+    # 当输入真实标签，返回loss值；无真实标签，返回预测值
+    def forward(self, x, y=None):
+        x = self.linear(x)  # (batch_size, input_size) -> (batch_size, output_size)
+        if y is not None:
+            return self.loss(x, y)  # 预测值和真实值计算损失
+        else:
+            return self.activation(x)  # 输出预测结果
+
+
+def build_sample():
+    x = np.random.random(5)
+    max_index = np.argmax(x)
+    return x, max_index
+
+
+def build_dataset(total_sample_num):
+    X = []
+    Y = []
+    for i in range(total_sample_num):
+        x, y = build_sample()
+        X.append(x)
+        Y.append(y)
+    return torch.FloatTensor(X), torch.LongTensor(Y)
+
+
+def evaluate(model):
+    model.eval()
+    test_sample_num = 100
+    x, y = build_dataset(test_sample_num)
+    correct, wrong = 0, 0
+    with torch.no_grad():
+        y_pred = model(x)
+        _, predicted = torch.max(y_pred, 1)
+        correct += (predicted == y).sum().item()
+        wrong = test_sample_num - correct
+    print("正确预测个数：%d, 正确率：%f" % (correct, correct / (correct + wrong)))
+    return correct / (correct + wrong)
+
+
+def main():
+    epoch_num = 20
+    batch_size = 20
+    train_sample = 5000
+    input_size = 5
+    output_size = 5
+    learning_rate = 0.001
+    model = TorchModel(input_size, output_size)
+    optim = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    log = []
+    train_x, train_y = build_dataset(train_sample)
+    for epoch in range(epoch_num):
+        model.train()
+        watch_loss = []
+        for batch_index in range(train_sample // batch_size):
+            x = train_x[batch_index * batch_size : (batch_index + 1) * batch_size]
+            y = train_y[batch_index * batch_size : (batch_index + 1) * batch_size]
+            loss = model(x, y)
+            loss.backward()
+            optim.step()
+            optim.zero_grad()
+            watch_loss.append(loss.item())
+        print("=========\n第%d轮平均loss:%f" % (epoch + 1, np.mean(watch_loss)))
+        acc = evaluate(model)
+        log.append([acc, float(np.mean(watch_loss))])
+    torch.save(model.state_dict(), Path(__file__).resolve().parent / "model.bin")
+    plt.plot(range(len(log)), [l[0] for l in log], label="acc")
+    plt.plot(range(len(log)), [l[1] for l in log], label="loss")
+    plt.legend()
+    plt.show()
+
+def predict(model_path, input_vec):
+    input_size = 5
+    output_size = 5
+    model = TorchModel(input_size, output_size)
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+    with torch.no_grad():
+        result = model.forward(torch.FloatTensor(input_vec))
+    for vec, res in zip(input_vec, result):
+        max_index = torch.argmax(res).item()
+        print("输入：%s, 预测类别：%d, 概率分布：%s" % (vec, max_index, res))
+
+if __name__ == "__main__":
+    main()
+    test_vec = [[0.97889086,0.15229675,0.31082123,0.03504317,0.88920843],
+                [0.74963533,0.5524256,0.95758807,0.95520434,0.84890681],
+                [0.00797868,0.67482528,0.13625847,0.34675372,0.19871392],
+                [0.09349776,0.59416669,0.92579291,0.41567412,0.1358894]]
+    predict(Path(__file__).resolve().parent / "model.bin", test_vec)
