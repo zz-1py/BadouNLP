b273aea37a361236d2429adfe2d44b7117875a06
fancy <fancy@ctrldoc.com>
2024-11-28 20:55:09 +0800

    Week02

---
+++ 肖芳/Week02/create_dataset.py
@@ -0,0 +1,18 @@
+import numpy as np
+import torch
+
+def build_sample():
+    x = np.random.random(5)
+    return x, np.argmax(x)
+
+# 随机生成一批样本
+# 正负样本均匀生成
+def build_dataset(total_sample_num):
+    X = []
+    Y = []
+    for i in range(total_sample_num):
+        x, y = build_sample()
+        X.append(x)
+        Y.append(y)
+    return torch.FloatTensor(X), torch.LongTensor(Y)
+    
